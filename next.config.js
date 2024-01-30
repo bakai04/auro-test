4de7e39a4f7077af82a982546f78d9ae09795154
bakai <joldoshovbakai04@gmail.com>
2024-01-30 10:32:28 +0600

    feat: fix deploy bags

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/i,
