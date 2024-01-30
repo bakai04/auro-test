70cfc0d445f88ee36d3e93625b344fa3afac9821
bakai <joldoshovbakai04@gmail.com>
2024-01-30 10:15:38 +0600

    feat: fix deploy bags

--- src/features/registration-progress/RegistrationProgress.tsx
+++ src/features/registration-progress/RegistrationProgress.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import { Wrapper } from "./styled";
 import { Progress, Text } from "@/shared/ui";
 
-export const RegistrationProgress = ({page,percent}) => {
+interface IRegistrationProgress {
+    page: number;
+    percent: number;
+}
+
+export const RegistrationProgress = ({
+    page,
+    percent,
+}: IRegistrationProgress) => {
     return (
         <Wrapper>
             <Text variant={"caption1"} color={"blue"}>
--- src/pages/palmistry/palms-hold.tsx
+++ src/pages/palmistry/palms-hold.tsx
@@ -1,38 +1,61 @@
-import React from 'react';
+import React from "react";
 import { Layout } from "@/widgets/layout";
 import { Header } from "@/widgets/header";
 import { LinkStyled, Wrapper } from "@/pages/palmistry/styled";
 import Flex from "@/shared/ui/flex/Flex";
 import { Button, Text } from "@/shared/ui";
 import { RegistrationProgress } from "@/features/registration-progress";
-import {AstralHand} from "@/shared/ui/icons";
+import { AstralHand } from "@/shared/ui/icons";
 
 const PalmsHold = () => {
-
-
     return (
         <Layout>
             <Header />
             <RegistrationProgress page={3} percent={33} />
             <Wrapper>
-                <Flex justifyContent={"center"} gap={"24px"} flex={"flex"} flexDirection={"column"}
-                      alignItems={"center"}>
-                    <AstralHand/>
-                    <Text variant={"caption2"} textCenter color={"midnightBlack"} lineHeight={"line28"}>
-                        Your palms hold a wealth of information about your fate and personality
+                <Flex
+                    justifyContent={"center"}
+                    gap={"24px"}
+                    flex={"flex"}
+                    flexDirection={"column"}
+                    alignItems={"center"}
+                >
+                    <AstralHand />
+                    <Text
+                        variant={"caption2"}
+                        textCenter
+                        color={"midnightBlack"}
+                        lineHeight={"line28"}
+                    >
+                        Your palms hold a wealth of information about your fate
+                        and personality
                     </Text>
-                    <Flex justifyContent={"center"} gap={"12px"} flex={"flex"} flexDirection={"column"}
-                          alignItems={"center"}>
-                        <Button type="button">
-                            Continue
-                        </Button>
-                        <Flex justifyContent={"center"} gap={"6px"} flex={"flex"} flexDirection={"column"}
-                              alignItems={"center"}>
-                            <Text variant={"caption3"} textCenter color={"midnightBlack"} lineHeight={"line18"}>
-                                By continuing, you agree to our <LinkStyled href="">EULA</LinkStyled> and <LinkStyled
-                                href="">Privacy
-                                Notice</LinkStyled>. Have a question? Reach our support team here <LinkStyled
-                                href="">here</LinkStyled>.
+                    <Flex
+                        justifyContent={"center"}
+                        gap={"12px"}
+                        flex={"flex"}
+                        flexDirection={"column"}
+                        alignItems={"center"}
+                    >
+                        <Button type="button">Continue</Button>
+                        <Flex
+                            justifyContent={"center"}
+                            gap={"6px"}
+                            flex={"flex"}
+                            flexDirection={"column"}
+                            alignItems={"center"}
+                        >
+                            <Text
+                                variant={"caption3"}
+                                textCenter
+                                color={"midnightBlack"}
+                                lineHeight={"line18"}
+                            >
+                                By continuing, you agree to our{" "}
+                                <LinkStyled href="">EULA</LinkStyled> and{" "}
+                                <LinkStyled href="">Privacy Notice</LinkStyled>.
+                                Have a question? Reach our support team here{" "}
+                                <LinkStyled href="">here</LinkStyled>.
                             </Text>
                         </Flex>
                     </Flex>
